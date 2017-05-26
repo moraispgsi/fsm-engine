4598b5790151719d345734551952a33bc3dca53b
moraispgsi <moraispgsi@gmail.com>
2017-05-26 01:52:47 +0100

    namespaces and redirection of actions

--- actions/actions.js
+++ actions/actions.js
@@ -1,56 +1,69 @@
 /**
  * Created by Ricardo Morais on 28/03/2017.
  */
+module.exports = function(actionsHost){
 
-let vm = require('vm');
-let request = require('request');
+    let vm = require('vm');
+    let request = require('request');
 
-/*
-high lever actions arguments come in this format
-    actionArguments: {
-       argName1: value1,
-       argName2: value2
-    }
-*/
+    /**
+     * Executes a custom action on an actions server host by calling its REST API /execute method
+     * @param namespace The namespace of the action
+     * @param action The name of the action
+     * @param sandbox The sandbox of the finite-state machine instance which the action was requested
+     * @param event The event in case the action was requested inside a transition
+     * @param actionArguments The arguments of the action call
+     */
+    let execute = function(namespace, action, sandbox, event, actionArguments) {
 
-let execute = function(action, sandbox, event, actionArguments) {
+        try {
 
-    let data = {
-        action: action,
-        arguments: {}
+            let data = {
+                stripNsPrefixRe: /^(?:{(?:[^}]*)})?(.*)$/,
+                namespace: namespace,
+                action: action,
+                arguments: {}
+            };
+
+            for (let key of Object.keys(actionArguments)) {
+                if(key[0] != '$'){
+                    if (key.startsWith("expr")) {
+                        let newKey = key.substring("expr".length);
+                        newKey = newKey[0].toLowerCase() + newKey.substring(1);
+                        data.arguments[newKey] = vm.runInContext(actionArguments[key], sandbox);
+                    } else {
+                        data.arguments[key] = actionArguments[key];
+                    }
+                }
+            }
+
+            let host = actionsHost;
+            let route = "/execute";
+            let errorEvent = actionArguments.errorEvent;
+            let successEvent = actionArguments.successEvent;
+
+            request({
+                url: host + route,
+                method: "POST",
+                json: true,
+                body: data,
+            }, (error, response, body) => {
+                if (error) {
+                    if (errorEvent) {
+                        this.raise(errorEvent);
+                    }
+                    return;
+                }
+                if (successEvent) {
+                    this.raise(successEvent);
+                }
+            });
+
+        } catch (err) {
+            console.log(err);
+        }
     };
 
-    for(let key of Object.keys(actionArguments)) {
-        if(key.startsWith("expr")) {
-            let newKey = actionArguments.key.substring("expr".length);
-            newKey = Character.toLowerCase(newKey[0])+newKey.substring(1);
-            data.arguments[newKey] = vm.runInContext(actionArguments[key], sandbox);
-        } else {
-            data.arguments[key] = actionArguments[key];
-        }
-    }
+    return execute;
 
-    let host = actionArguments.host;
-    let route = "/execute";
-    let errorEvent = actionArguments.errorEvent;
-    let successEvent = actionArguments.successEvent;
-
-    request({
-        url: host + route,
-        method: "POST",
-        json: true,
-        body: data,
-    }, (error, response, body) => {
-        if (error) {
-            if(errorEvent) {
-                this.raise(errorEvent);
-            }
-            return;
-        }
-        if(successEvent) {
-            this.raise(successEvent);
-        }
-    });
 };
-
-module.exports = execute;
